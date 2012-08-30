dce5e783da3c593f3cd94933f2bf933f0bb2c6db
Dan Kantor <dan@ex.fm>
2012-08-30 11:42:48 -0400

    consolidated common functions

--- test/playqueue.test.js
+++ test/playqueue.test.js
@@ -8,25 +8,47 @@ var songUrlTwo = 'http://serve.castfire.com/audio/1129506/'+
     'solos-carpe-diem_2012-08-24-172906.128.mp3';
 var songUrlThree = 'http://serve.castfire.com/audio/1129504/'+
     'fierce-creatures-catacomb-party_2012-08-24-172704.128.mp3';
+    
+function createNewPlayQueue(){
+    var pq = new PlayQueue(
+        {
+            'audio' : new Audio()
+        }
+    );
+    return pq;
+}
+
+function addSongs(pq){
+    pq.add([
+        {
+            'id': '0',
+            'url': songUrlZero
+        },
+        {
+            'id': '1',
+            'url': songUrlOne
+        },
+        {
+            'id': '2',
+            'url': songUrlTwo
+        },
+        {
+            'id': '3',
+            'url': songUrlThree
+        }
+    ]);
+}
 
 describe("PlayQueue", function(){
     describe("list", function(){
         describe("main", function(){
             it("should have an internal array to hold songs", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
+                var pq = createNewPlayQueue();
                 assert.equal(typeof(pq.getList()), 'object');
                 assert.equal(pq.getList().length, 0);
             });
             it("should return the array on getList call", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
+                var pq = createNewPlayQueue();
                 pq.add(songUrlZero);
                 assert.equal(JSON.stringify(pq.getList()), 
                     '[{"url":"'+songUrlZero+'","_listPosition":0}]');
@@ -35,11 +57,7 @@ describe("PlayQueue", function(){
         
         describe("add", function(){
             it("should add an array of song objects to list", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
+                var pq = createNewPlayQueue();
                 pq.add([
                     {
                         'id': '0',
@@ -53,11 +71,7 @@ describe("PlayQueue", function(){
                 assert.equal(pq.getList().length, 2);
             });
             it("should add a song object to list", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
+                var pq = createNewPlayQueue();
                 pq.add({
                     'id': '0',
                     'url': songUrlZero
@@ -65,11 +79,7 @@ describe("PlayQueue", function(){
                 assert.equal(pq.getList().length, 1);
             });
             it("should create new song object with given url string and add to list", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
+                var pq = createNewPlayQueue();
                 pq.add(songUrlZero);
                 assert.equal(pq.getList().length, 1);
                 assert.equal(typeof(pq.getList()[0]), 'object');
@@ -78,44 +88,18 @@ describe("PlayQueue", function(){
 
         describe("remove", function(){
             it("should remove an item at a specific index", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    }
-                ]);
-                assert.equal(pq.getList().length, 2);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
+                assert.equal(pq.getList().length, 4);
                 pq.remove(1);
-                assert.equal(pq.getList().length, 1);
+                assert.equal(pq.getList().length, 3);
                 pq.remove(0);
-                assert.equal(pq.getList().length, 0);
+                assert.equal(pq.getList().length, 2);
             });
             it("should clear all items", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    }
-                ]);
-                assert.equal(pq.getList().length, 2);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
+                assert.equal(pq.getList().length, 4);
                 pq.clear();
                 assert.equal(pq.getList().length, 0);
                 assert.equal(pq.getQueueNumber(), 0);
@@ -126,50 +110,16 @@ describe("PlayQueue", function(){
         
         describe("move", function(){
             it("should move a song from one position to another", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    }
-                ]);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
                 pq.move(0, 1);
                 assert.equal(pq.getList()[1].url, songUrlZero);
                 assert.equal(pq.getList()[1].id, '0');
                 assert.equal(pq.getList()[1]._listPosition, 1);
             });
             it("should re-order all songs effected after one moves", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    },
-                    {
-                        'id': '2',
-                        'url': songUrlTwo
-                    },
-                    {
-                        'id': '3',
-                        'url': songUrlThree
-                    }
-                ]);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
                 pq.move(1, 2);
                 assert.equal(pq.getList()[0].url, songUrlZero);
                 assert.equal(pq.getList()[1].url, songUrlTwo);
@@ -181,21 +131,8 @@ describe("PlayQueue", function(){
                 assert.equal(pq.getList()[3]._listPosition, 3);
             });
             it("not allow you to move a song to the same position", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    }
-                ]);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
                 try{
                     pq.move(0, 0);
                 }catch(e){
@@ -203,21 +140,8 @@ describe("PlayQueue", function(){
                 }
             });
             it("not allow you to move a song to a position greater than the list length", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    }
-                ]);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
                 try{
                     pq.move(0, 3);
                 }catch(e){
@@ -225,27 +149,15 @@ describe("PlayQueue", function(){
                 }
             });
             it("not allow you to move a song in a position greater than the list length", function(){
-                var pq = new PlayQueue(
-                    {
-                        'audio' : new Audio()
-                    }
-                );
-                pq.add([
-                    {
-                        'id': '0',
-                        'url': songUrlZero
-                    },
-                    {
-                        'id': '1',
-                        'url': songUrlOne
-                    }
-                ]);
+                var pq = createNewPlayQueue();
+                addSongs(pq);
                 try{
                     pq.move(3, 0);
                 }catch(e){
                     assert.equal(e.message, "itemIndex out of bounds");
                 }
             });
-        });
+        }); // end move
+        
     }); // end list
 });
